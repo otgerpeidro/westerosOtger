95dd08a3d79ba316efa84dde9abb92e00e7be2dd
Otger Peidro <otgerpeidro@MacBook-Pro-de-Otger.local>
2017-10-11 20:02:58 +0200

    empty app

--- Westeros/AppDelegate.swift
+++ Westeros/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Crear Window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = UIColor.cyan
+        
+        // Crear RootVC
+        let rootVC = UIViewController()
+        window?.rootViewController = rootVC
+        
         return true
     }
 
